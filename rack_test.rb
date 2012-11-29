90e6fb08af221b8c4f4fdc3064ecf66fefebe7a3
Tomohiko Himura <eiel.hal@gmail.com>
2012-11-29 12:18:15 +0900

    中身の処理を body メソッドに移動

--- rack_test.rb
+++ rack_test.rb
@@ -4,26 +4,27 @@ require 'erb'
 
  class RackApp
    def call(env)
-     [200, 
-      {'Content-Type' => 'text/html'}, 
-      ['<html>
+     [ 200, {'Content-Type' => 'text/html'}, [body] ]
+   end
+
+   def body
+     '<html>
         <head><title>Rack test</title></head>
         <body>
         <h1>Rack test</h1>
-        <p> 
-           <% (1..9).each do |y| %>  
-           <%= y %>
+        <p>
+           <% (1..9).each do |y| %>
+             <%= y %>
            <% end %>
            <%(1..9).each do |x|%>
-           <%= x %>
-           <%(1..9).each do |y|%>
-           <%= %>
-           <% end %>
+             <%= x %>
+             <%(1..9).each do |y|%>
+               <%= x*y %>
+             <% end %>
           <% end %>
         </p>
         </body>
         </html>
-        ']
-        ]
-    end
+        '
+   end
  end
